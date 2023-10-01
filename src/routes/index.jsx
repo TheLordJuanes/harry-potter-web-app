9fec51ab4e53deb260e2acc7c9274099d48a7c8f
Juan Esteban Caicedo Alzate <juancaicedo2000@gmail.com>
2023-09-30 20:10:16 -0500

    Designed the base UI of the Characters page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "../pages/Login.jsx";
 import Home from "../pages/Home.jsx";
+import Characters from "../pages/Characters.jsx";
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/"  element={<Login />}/>
             <Route path="/home"  element={<Home />}/>
+            <Route path="/characters"  element={<Characters/>}/>
         </Routes>
     </BrowserRouter>
 )
