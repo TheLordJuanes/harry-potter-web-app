bfc77983aea701d12061fbc5465febac438f8978
Juan Esteban Caicedo Alzate <juancaicedo2000@gmail.com>
2023-10-01 14:54:25 -0500

    Implemented a modal to show the potion details

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -17,38 +17,38 @@ export default function MovieCard({movie}) {
     return (
         <>
             <Card className="max-w-sm mx-auto overflow-hidden shadow-lg" side="lg">
-                <Card.Image src={movie.attributes.poster} alt="Movie poster"/>
+                <Card.Image src={movie.poster} alt="Movie poster"/>
                 <Card.Body>
-                    <Card.Title tag="h2">{movie.attributes.title}</Card.Title>
-                    <p>Rating: {movie.attributes.rating !== null ? <Badge color="success">{movie.attributes.rating}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                    <p>Release date: {movie.attributes.release_date !== null ? <Badge color="accent">{movie.attributes.release_date}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                    <p>Running time: {movie.attributes.running_time !== null ? <Badge color="info">{movie.attributes.running_time}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                    <Card.Title tag="h2">{movie.title}</Card.Title>
+                    <p>Rating: {movie.rating !== null ? <Badge color="success">{movie.rating}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                    <p>Release date: {movie.release_date !== null ? <Badge color="accent">{movie.release_date}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                    <p>Running time: {movie.running_time !== null ? <Badge color="info">{movie.running_time}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
                     <Card.Actions className="justify-end">
                         <div className="font-sans">
                             <Button color="primary" onClick={handleShow}>Details</Button>
                             <Modal ref={ref}>
-                                <Modal.Header className="font-bold text-center">{movie.attributes.title}</Modal.Header>
+                                <Modal.Header className="font-bold text-center">{movie.title}</Modal.Header>
                                 <Modal.Body>
-                                    <img src={movie.attributes.poster !== null ? movie.attributes.poster : "https://potterdb.com/images/missing_character.svg"} className="max-w-sm rounded-lg shadow-2xl mx-auto"  alt="Character image"/>
+                                    <img src={movie.poster !== null ? movie.poster : "https://potterdb.com/images/missing_character.svg"} className="max-w-sm rounded-lg shadow-2xl mx-auto"  alt="Character image"/>
                                     <br/>
                                     <h2 className="text-2xl font-bold">Summary</h2>
                                     <br/>
-                                    <p className="text-justify">{movie.attributes.summary !== null ? movie.attributes.summary : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-justify">{movie.summary !== null ? movie.summary : <Badge color="neutral">Unknown</Badge>}</p>
                                     <br/>
-                                    <p className="text-lg">Rating: {movie.attributes.rating !== null ? <Badge color="success">{movie.attributes.rating}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Release date: {movie.attributes.release_date !== null ? <Badge color="accent">{movie.attributes.release_date}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Running time: {movie.attributes.running_time !== null ? <Badge color="info">{movie.attributes.running_time}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Trailer: {movie.attributes.trailer !== null ? movie.attributes.trailer : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Budget: {movie.attributes.budget !== null ? <Badge color="success">{movie.attributes.budget}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Box office: {movie.attributes.box_office !== null ? <Badge color="warning">{movie.attributes.box_office}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Directors: {movie.attributes.directors !== null ? <Badge color="info">{movie.attributes.directors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Cinematographers: {movie.attributes.cinematographers !== null ? <Badge color="warning">{movie.attributes.cinematographers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Editors: {movie.attributes.editors !== null ? <Badge color="info">{movie.attributes.editors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Distributors: {movie.attributes.distributors !== null ? <Badge color="secondary">{movie.attributes.distributors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Music composers: {movie.attributes.music_composers !== null ? <Badge color="accent">{movie.attributes.music_composers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Producers: {movie.attributes.producers !== null ? <Badge color="primary">{movie.attributes.producers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Screenwriters: {movie.attributes.screenwriters !== null && movie.attributes.screenwriters.length > 0 ? <Badge color="success">{movie.attributes.screenwriters}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
-                                    <p className="text-lg">Wiki: {movie.attributes.wiki !== null ? movie.attributes.wiki : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Rating: {movie.rating !== null ? <Badge color="success">{movie.rating}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Release date: {movie.release_date !== null ? <Badge color="accent">{movie.release_date}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Running time: {movie.running_time !== null ? <Badge color="info">{movie.running_time}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Trailer: {movie.trailer !== null ? movie.trailer : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Budget: {movie.budget !== null ? <Badge color="success">{movie.budget}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Box office: {movie.box_office !== null ? <Badge color="warning">{movie.box_office}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Directors: {movie.directors !== null ? <Badge color="info">{movie.directors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Cinematographers: {movie.cinematographers !== null ? <Badge color="warning">{movie.cinematographers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Editors: {movie.editors !== null ? <Badge color="info">{movie.editors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Distributors: {movie.distributors !== null ? <Badge color="secondary">{movie.distributors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Music composers: {movie.music_composers !== null ? <Badge color="accent">{movie.music_composers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Producers: {movie.producers !== null ? <Badge color="primary">{movie.producers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Screenwriters: {movie.screenwriters !== null && movie.screenwriters.length > 0 ? <Badge color="success">{movie.screenwriters}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Wiki: {movie.wiki !== null ? movie.wiki : <Badge color="neutral">Unknown</Badge>}</p>
                                 </Modal.Body>
                                 <Modal.Actions>
                                     <Button onClick={handleClose}>Close</Button>
--- src/components/PotionCard.jsx
+++ src/components/PotionCard.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useCallback, useRef} from 'react';
 import PropTypes from "prop-types";
-import {Badge, Button, Card} from "react-daisyui";
+import {Badge, Button, Card, Modal} from "react-daisyui";
 
 export default function PotionCard({potion}) {
+
+    const ref = useRef(null);
+
+    const handleShow = useCallback(() => {
+        ref.current?.showModal();
+    }, [ref]);
+
+    const handleClose = () => {
+        ref.current?.close()
+    }
+
     return (
         <>
             <Card className="max-w-sm mx-auto overflow-hidden shadow-lg" side="lg">
@@ -13,7 +24,28 @@ export default function PotionCard({potion}) {
                     <p>Difficulty: {potion.difficulty !== null ? (potion.difficulty === 'Advanced' || potion.difficulty === 'One of a kind' || potion.difficulty === 'Advanced or Beginner' ? <Badge color="error">{potion.difficulty}</Badge> : (potion.difficulty === 'Moderate' || potion.difficulty === 'Moderate to Advanced' || potion.difficulty === 'Moderate to advanced' || potion.difficulty === 'Moderate/Advanced' ? <Badge color="warning">{potion.difficulty}</Badge> : <Badge color="info">{potion.difficulty}</Badge>)) : <Badge color="neutral">Unknown</Badge>}</p>
                     <p>Characteristics: {potion.characteristics !== null ? potion.characteristics : <Badge color="neutral">Unknown</Badge>}</p>
                     <Card.Actions className="justify-end">
-                        <Button color="primary">Details</Button>
+                        <div className="font-sans">
+                            <Button color="primary" onClick={handleShow}>Details</Button>
+                            <Modal ref={ref}>
+                                <Modal.Header className="font-bold text-center">{potion.name}</Modal.Header>
+                                <Modal.Body>
+                                    <img src={potion.image !== null ? potion.image : "https://potterdb.com/images/missing_character.svg"} className="max-w-sm rounded-lg shadow-2xl mx-auto"  alt="Character image"/>
+                                    <br/>
+                                    <p className="text-lg">Effect: {potion.effect !== null ? potion.effect : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Difficulty: {potion.difficulty !== null ? (potion.difficulty === 'Advanced' || potion.difficulty === 'One of a kind' || potion.difficulty === 'Advanced or Beginner' ? <Badge color="error">{potion.difficulty}</Badge> : (potion.difficulty === 'Moderate' || potion.difficulty === 'Moderate to Advanced' || potion.difficulty === 'Moderate to advanced' || potion.difficulty === 'Moderate/Advanced' ? <Badge color="warning">{potion.difficulty}</Badge> : <Badge color="info">{potion.difficulty}</Badge>)) : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Characteristics: {potion.characteristics !== null ? potion.characteristics : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Ingredients: {potion.ingredients !== null ? potion.ingredients : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Side effects: {potion.side_effects !== null ? potion.side_effects : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Time: {potion.time !== null ? <Badge color="warning">{potion.time}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Inventors: {potion.inventors !== null ? <Badge color="info">{potion.inventors}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Manufacturers: {potion.manufacturers !== null ? <Badge color="error">{potion.manufacturers}</Badge> : <Badge color="neutral">Unknown</Badge>}</p>
+                                    <p className="text-lg">Wiki: {potion.wiki !== null ? potion.wiki : <Badge color="neutral">Unknown</Badge>}</p>
+                                </Modal.Body>
+                                <Modal.Actions>
+                                    <Button onClick={handleClose}>Close</Button>
+                                </Modal.Actions>
+                            </Modal>
+                        </div>
                     </Card.Actions>
                 </Card.Body>
             </Card>
