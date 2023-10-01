c750edffa390a898c13457302d47b676d485ae7b
Juan Pablo Ramos Torres <378tech@gmail.com>
2023-10-01 00:21:29 -0500

    Firebase was integrated in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css"
 import Router from "./routes";
+import { useFirebaseApp } from "reactfire";
 
 export default function App() {
+  const firebase = useFirebaseApp();
+  console.log(firebase);
   return (
       <>
           <Router/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { FirebaseAppProvider } from "reactfire";
+import firebaseConfig from './firebase-config.js';
+import {Loading} from 'react-daisyui'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+            <Suspense fallback={<Loading/>}>
+                <App />
+            </Suspense>
+        </FirebaseAppProvider>
     </React.StrictMode>,
 )
